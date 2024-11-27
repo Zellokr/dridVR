67c371a3140e1ec2c084f1ae135485b1100b1e70
Kristian <kristianmartinezgarcia@gmail.com>
2024-11-27 15:26:17 +0000

    refactor code

--- components/Features/Features.vue
+++ components/Features/Features.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, computed, onMounted } from 'vue';
-import gamesData from '@/public/features.json';
+import featureGames from '@/public/features.json';
 
 interface Game {
   name?: string
@@ -9,16 +9,7 @@ interface Game {
   yt_url?: string
 }
 
-const games = ref<Game[]>(gamesData);
-const hasVideo = ref(false)
-
-const containVideo = computed(() => {
-  if(hasVideo.value)
-    return gamesData.filter((game: Game) => game.yt_url !== '')
-  else
-    return games.value;
-
-})
+const games = ref<Game[]>(featureGames);
 
 </script>
 
@@ -27,6 +18,6 @@ const containVideo = computed(() => {
     <div class="flex flex-col justify-center items-center">
       <h1 class="text-white text-5xl font-bold py-8 mb-2">Destacados</h1>
     </div>
-    <CardGames :game-records="containVideo" />
+    <CardGames :game-records="games" />
   </div>
 </template>
