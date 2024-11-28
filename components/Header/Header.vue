092ea27ead16b3301b0d91939f04246f97cf29f9
Kristian <kristianmartinezgarcia@gmail.com>
2024-11-28 22:57:57 +0000

    change discount and color header

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -7,8 +7,8 @@ const goToChannel = () => {
 </script>
 
 <template>
-  <div class="bg-gray-800 h-auto w-full pb-4">
-    <div class="flex flex-col justify-center items-center gap-y-5 sm:gap-y-3 md:gap-x-8 md:py-8">
+  <div class="bg-gray-700 h-auto w-full pb-4 rounded-b-full">
+    <div class="flex flex-col justify-center items-center gap-y-5 sm:gap-y-3 md:gap-x-8 md:py-8 ">
       <!-- Logo -->
       <img
           src="/img/logo.png"
@@ -21,7 +21,7 @@ const goToChannel = () => {
       <!-- Texto -->
       <div class="text-center md:text-left px-5 md:px-2">
       <span class="text-white text-base sm:text-lg md:text-xl font-bold leading-relaxed">
-        ¡Usa estos enlaces de afiliado y consigue un <span class="text-yellow-400">30% de descuento</span> en cualquier juego!
+        ¡Usa estos enlaces de afiliado y consigue un <span class="text-yellow-400">20% de descuento</span> en cualquier juego!
       </span>
       </div>
     </div>
