d241f4f9486710b726713add8c7f9b232b91b1f0
Kristian <kristianmartinezgarcia@gmail.com>
2024-12-02 12:30:19 +0000

    change grid column feature

--- components/Features/Features.vue
+++ components/Features/Features.vue
@@ -6,7 +6,7 @@ import featureGames from "@/public/features.json";
   <div class="py-4">
     <TitleContent title="Novedades" class="flex flex-col gap-y-2" />
     <div
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-4"
     >
       <div v-for="game in featureGames" :key="game.id">
         <Card :game="game" />
