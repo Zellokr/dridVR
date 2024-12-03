958bce0b8d9fae60a57e55572f24167d9523a58e
Kristian <kristianmartinezgarcia@gmail.com>
2024-12-03 20:49:42 +0000

    add title and meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,37 @@ export default defineNuxtConfig({
   ssr: false,
   app: {
     head: {
+      meta: [
+        {
+          name: "juegos baratos",
+          content: "Los juegos más baratos de internet",
+        },
+        {
+          name: "juegos VR",
+          content: "Videojuegos de realidad virtual",
+        },
+        {
+          name: "Meta Quest",
+          content: "Dispositivos de realidad virtual Meta",
+        },
+        {
+          name: "Meta Quest Store",
+          content: "Compra dispositivos Meta Quest",
+        },
+        {
+          name: "Accesorios realidad virtual",
+          content: "Compra accesorios Meta Quest de realidad virtual",
+        },
+        {
+          name: "Compra videojuegos de realidad virtual",
+          content: "Compra videojuegos Meta Quest de realidad virtual",
+        },
+        {
+          name: "Compra videojuegos baratos de realidad virtual",
+          content: "Compra videojuegos baratos Meta Quest de realidad virtual",
+        },
+      ],
+      title: "Los mejores juegos de VR baratos",
       link: [
         { rel: "preload", href: "/img/banner_image_1.webp", as: "image" },
         { rel: "preload", href: "/img/banner_image_2.webp", as: "image" },
