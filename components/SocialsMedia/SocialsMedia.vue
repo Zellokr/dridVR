9a68fef973ecce6c3a6d333ec8870f9ecb643735
Kristian <kristianmartinezgarcia@gmail.com>
2024-12-05 21:57:00 +0000

    fix socialMedia warning

--- components/SocialsMedia/SocialsMedia.vue
+++ components/SocialsMedia/SocialsMedia.vue
@@ -3,28 +3,30 @@ import { handleContent } from "~/utils/handleContent";
 </script>
 
 <template>
-  <div class="flex space-x-3.5">
-    <transition-group name="scale">
-      <MdiIcon
-        icon="mdiInstagram"
-        class="text-white w-8 h-8 cursor-pointer transition-transform duration-200 hover:scale-125"
-        @click="handleContent('https://www.instagram.com/dridgg/')"
-      />
-      <MdiIcon
-        icon="mdiTwitter"
-        class="text-white w-8 h-8 cursor-pointer transition-transform duration-200 hover:scale-125"
-        @click="handleContent('https://twitter.com/DriDGG')"
-      />
-      <MdiIcon
-        icon="mdiYoutube"
-        class="text-white w-8 h-8 cursor-pointer transition-transform duration-200 hover:scale-125"
-        @click="handleContent('https://www.youtube.com/@dridvr')"
-      />
-      <MdiIcon
-        icon="mdiTwitch"
-        class="text-white w-8 h-8 cursor-pointer transition-transform duration-200 hover:scale-125"
-        @click="handleContent('https://www.twitch.tv/dridgg')"
-      />
-    </transition-group>
-  </div>
+  <transition-group name="scale" tag="div" class="flex space-x-3.5">
+    <MdiIcon
+      key="instagram"
+      icon="mdiInstagram"
+      class="text-white w-8 h-8 cursor-pointer transition-transform duration-200 hover:scale-125"
+      @click="handleContent('https://www.instagram.com/dridgg/')"
+    />
+    <MdiIcon
+      key="twitter"
+      icon="mdiTwitter"
+      class="text-white w-8 h-8 cursor-pointer transition-transform duration-200 hover:scale-125"
+      @click="handleContent('https://twitter.com/DriDGG')"
+    />
+    <MdiIcon
+      key="youtube"
+      icon="mdiYoutube"
+      class="text-white w-8 h-8 cursor-pointer transition-transform duration-200 hover:scale-125"
+      @click="handleContent('https://www.youtube.com/@dridvr')"
+    />
+    <MdiIcon
+      key="twitch"
+      icon="mdiTwitch"
+      class="text-white w-8 h-8 cursor-pointer transition-transform duration-200 hover:scale-125"
+      @click="handleContent('https://www.twitch.tv/dridgg')"
+    />
+  </transition-group>
 </template>
