9c8ab9b633c44f63766e3164713a17f5a8122e09
DriDGG <yeray1060@gmail.com>
2025-01-15 10:37:56 +0000

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -1,16 +1,17 @@
 import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
-  {
-    image: "img/Banner/grimbanner.jpg",
-    altText: "Videojuego de realidad virtual llamado GRIM",
-    url: "https://www.tkqlhce.com/click-101284762-15639798?url=https%3A%2F%2Fwww.meta.com%2Fexperiences%2F6488902397838458%2F&cjsku=6488902397838458",
-  },
+  
   {
     image: "img/Banner/banner_image_1.webp",
     altText: "Banner de videojuegos",
     url: "/games",
     contentId: "",
+  },
+  {
+    image: "img/Banner/grimbanner.jpg",
+    altText: "Videojuego de realidad virtual llamado GRIM",
+    url: "https://www.tkqlhce.com/click-101284762-15639798?url=https%3A%2F%2Fwww.meta.com%2Fexperiences%2F6488902397838458%2F&cjsku=6488902397838458",
   },
   {
     image: "img/Banner/banner_image_2.webp",
