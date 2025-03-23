2183eca5c50c556a725c27cd399b0ee31f8ab82d
DriDGG <yeray1060@gmail.com>
2025-03-23 16:49:14 +0000

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -1,11 +1,35 @@
 import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 
 export const featureGames: GameSchema[] = [
+   {
+    "name": "Hellsplit: Arena",
+    "affiliate_link": "https://www.meta.com/affiliates/ETUegrnYQ",
+    "img_link": "./img/Games/473392019_679491944433263_2769409538974515627_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 1
+  },
+  {
+    "name": "Room Escape",
+    "affiliate_link": "https://www.meta.com/affiliates/EMHjhVL8S",
+    "img_link": "./img/Games/38982556_2844243415743519_5900441646358582691_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
   {
     "name": "Path of Fury - Episode I: Tetsuo's Tower",
     "affiliate_link": "https://www.meta.com/affiliates/9uuEUvNNc",
     "img_link": "./img/Games/39003251_887905526469693_7022835035658312726_n.jpg",
     "yt_link": "https://youtu.be/IsoL98puCvU",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+  {
+    "name": "Mythic Realms",
+    "affiliate_link": "https://www.ftjcfx.com/click-101284762-15639798?url=https%3A%2F%2Fwww.meta.com%2Fexperiences%2F8364993590228690%2F&cjsku=8364993590228690&widgetid=67409c6d6e73e5d8cacefdc1",
+    "img_link": "./img/Games/38982616_818876450429225_6183193778885571007_n.jpg",
+    "yt_link": "https://youtu.be/YvIremtelJQ",
     "crossbuy": 0,
     "bhaptics": 0
   },
@@ -16,31 +40,7 @@ export const featureGames: GameSchema[] = [
     "yt_link": "https://youtu.be/ysNL-p9ymUY",
     "crossbuy": 0,
     "bhaptics": 0
-  },
-  {
-    "name": "Symphoni",
-    "affiliate_link": "https://www.meta.com/affiliates/DPcKYKFRe",
-    "img_link": "./img/Games/473391904_3707250382829048_3886230964039064979_n.jpg",
-    "yt_link": "",
-    "crossbuy": false,
-    "bhaptics": false,
-  },
-   {
-    "name": "Rogue Piñatas: VRMageddon",
-    "affiliate_link": "https://www.meta.com/affiliates/A47a9B9LR",
-    "img_link": "./img/Games/38974573_2438397013163309_3925724153479588440_n.jpg",
-    "yt_link": "",
-    "crossbuy": false,
-    "bhaptics": false,
-  },
-  
-  {
-    name: "Farming Simulator VR",
-    affiliate_link: "https://www.meta.com/affiliates/AL1Qk8tz4",
-    img_link: "./img/Games/39035391_475553498837357_5557784264540528780_n.jpg",
-    yt_link: "https://youtu.be/XRAFS3p6lK4",
-    crossbuy: false,
-    bhaptics: false,
   }
+  
 
 ];
