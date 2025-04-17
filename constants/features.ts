d89c5eb909abba9e817fe8aabc45ff02993f4d96
DriDGG <yeray1060@gmail.com>
2025-04-17 21:23:12 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -1,7 +1,24 @@
 import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 
 export const featureGames: GameSchema[] = [
-{
+
+    {
+    "name": "Rival Stars Horse Racing: Edición RV",
+    "affiliate_link": "https://www.meta.com/affiliates/4kAGybzBQ",
+    "img_link": "./img/Games/39036681_1463230354372521_7162511934584671949_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+    {
+    "name": "GORN 2",
+    "affiliate_link": "https://www.meta.com/affiliates/BMzsUVsLu",
+    "img_link": "./img/Games/473391974_941044658239557_467380685610637309_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+    {
     "name": "Sid Meier's Civilization® VII - VR",
     "affiliate_link": "https://www.meta.com/affiliates/ED1G655iA",
     "img_link": "./img/Games/473391887_8440619972704489_4528435562144445268_n.jpg",
@@ -21,23 +38,6 @@ export const featureGames: GameSchema[] = [
     "name": "Cooking Clash",
     "affiliate_link": "https://www.meta.com/affiliates/2HVSnojyji",
     "img_link": "./img/Games/38974524_1141405707608281_1577521188320323765_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-{
-    "name": "Crystal Commanders",
-    "affiliate_link": "https://www.ftjcfx.com/click-101284762-15639798?url=https%3A%2F%2Fwww.meta.com%2Fexperiences%2F24406315732346709%2F&cjsku=24406315732346709&widgetid=67409c6d6e73e5d8cacefdc1",
-    "img_link": "./img/Games/75439601_1500016790633227_1867518665895769982_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-   
-    {
-    "name": "Harpagun",
-    "affiliate_link": "https://www.meta.com/affiliates/AG2xWU51j",
-    "img_link": "./img/Games/473393450_1373422160353174_6191879637626026118_n.jpg",
     "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
