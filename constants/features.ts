a1d9bb1188c0d02905113ac8b1d5c29bb50113ae
DriDGG <yeray1060@gmail.com>
2025-04-21 16:34:47 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -3,26 +3,26 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 export const featureGames: GameSchema[] = [
 
     {
-    "name": "Rival Stars Horse Racing: Edición RV",
-    "affiliate_link": "https://www.meta.com/affiliates/4kAGybzBQ",
-    "img_link": "./img/Games/39036681_1463230354372521_7162511934584671949_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-    {
     "name": "GORN 2",
     "affiliate_link": "https://www.meta.com/affiliates/BMzsUVsLu",
     "img_link": "./img/Games/473391974_941044658239557_467380685610637309_n.jpg",
+    "yt_link": "https://youtu.be/1Kyb6GT_1gQ",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+    {
+    "name": "Harpagun",
+    "affiliate_link": "https://www.meta.com/affiliates/AG2xWU51j",
+    "img_link": "./img/Games/473393450_1373422160353174_6191879637626026118_n.jpg",
     "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
   },
     {
-    "name": "Sid Meier's Civilization® VII - VR",
-    "affiliate_link": "https://www.meta.com/affiliates/ED1G655iA",
-    "img_link": "./img/Games/473391887_8440619972704489_4528435562144445268_n.jpg",
-    "yt_link": "https://youtu.be/-ojiYXrEYKQ?si=vn6wGFfBuUkWci8l",
+    "name": "Cooking Clash",
+    "affiliate_link": "https://www.meta.com/affiliates/2HVSnojyji",
+    "img_link": "./img/Games/38974524_1141405707608281_1577521188320323765_n.jpg",
+    "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
   },
@@ -35,9 +35,9 @@ export const featureGames: GameSchema[] = [
     "bhaptics": 0
   },
  {
-    "name": "Cooking Clash",
-    "affiliate_link": "https://www.meta.com/affiliates/2HVSnojyji",
-    "img_link": "./img/Games/38974524_1141405707608281_1577521188320323765_n.jpg",
+    "name": "Magitek",
+    "affiliate_link": "https://www.meta.com/affiliates/K8Wm8dQEC",
+    "img_link": "./img/Games/75404383_386969957427569_8522271678978038126_n.jpg",
     "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
