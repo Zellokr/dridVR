7b37dd2916d802d558752828d0fa2e9eec5ddcf2
DriDGG <yeray1060@gmail.com>
2025-04-24 20:21:09 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -1,13 +1,11 @@
 import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
-  {
-    image: "img/Banner/Exfilzone.webp",
-    altText: "Banner de videojuegos",
-    url: "https://www.meta.com/affiliates/5UwuzwgNo",
-    contentId: "",
+   {
+    image: "img/Banner/ghosttownbanner.webp",
+    altText: "Videojuego de realidad virtual llamado Ghost Town",
+    url: "https://www.meta.com/affiliates/6faeBg6Ke",
   },
-  
   {
     image: "img/Banner/banner_image_1.webp",
     altText: "Banner de videojuegos",
@@ -15,9 +13,10 @@ export const bannerImagesList: DataImage[] = [
     contentId: "",
   },
   {
-    image: "img/Banner/bannerciv7.webp",
-    altText: "Videojuego de realidad virtual llamado CIVILIZATION",
-    url: "https://www.meta.com/affiliates/ED1G655iA",
+    image: "img/Banner/Exfilzone.webp",
+    altText: "Banner de videojuegos",
+    url: "https://www.meta.com/affiliates/5UwuzwgNo",
+    contentId: "",
   },
   {
     image: "img/Banner/banner_image_2.webp",
@@ -25,9 +24,9 @@ export const bannerImagesList: DataImage[] = [
     url: "https://www.meta.com/affiliates/DQCXmgkma",
   },
 
-  {
-    image: "img/Banner/wandererbanner.jpg",
-    altText: "Videojuego de realidad virtual llamado Wanderer",
-    url: "https://www.meta.com/affiliates/A4H6uARZ1",
+   {
+    image: "img/Banner/bannerciv7.webp",
+    altText: "Videojuego de realidad virtual llamado CIVILIZATION",
+    url: "https://www.meta.com/affiliates/ED1G655iA",
   }
 ];
