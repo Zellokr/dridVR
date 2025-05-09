5c32789315b31f32fde73d6e39fd262069fc3832
DriDGG <yeray1060@gmail.com>
2025-05-09 09:44:41 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -3,6 +3,14 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 export const featureGames: GameSchema[] = [
 
     {
+    "name": "Surviving Mars: Pioneer",
+    "affiliate_link": "https://www.meta.com/affiliates/CRwnkMcAK",
+    "img_link": "./img/Games/491830915_1634861297163168_4285532260596147144_n.jpg",
+    "yt_link": "https://youtu.be/itxnMdIPEFI",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+    {
     "name": "Ghost Town",
     "affiliate_link": "https://www.meta.com/affiliates/6faeBg6Ke",
     "img_link": "./img/Games/473391899_1346443866774466_5902162057010451886_n.jpg",
@@ -23,14 +31,6 @@ export const featureGames: GameSchema[] = [
     "affiliate_link": "https://www.meta.com/affiliates/BMzsUVsLu",
     "img_link": "./img/Games/473391974_941044658239557_467380685610637309_n.jpg",
     "yt_link": "https://youtu.be/1Kyb6GT_1gQ",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-    {
-    "name": "Harpagun",
-    "affiliate_link": "https://www.meta.com/affiliates/AG2xWU51j",
-    "img_link": "./img/Games/473393450_1373422160353174_6191879637626026118_n.jpg",
-    "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
   },
