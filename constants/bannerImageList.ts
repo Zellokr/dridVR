e404bc65f960f2ee43969de661670786134cdb22
DriDGG <yeray1060@gmail.com>
2025-05-27 17:30:00 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -1,10 +1,11 @@
 import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
-   {
-    image: "img/Banner/ghosttownbanner.webp",
-    altText: "Videojuego de realidad virtual llamado Ghost Town",
-    url: "https://www.meta.com/affiliates/6faeBg6Ke",
+   
+    {
+    image: "img/Banner/IntotheradiusBanner.webp",
+    altText: "Videojuego de realidad virtual llamado Into The Radius 2",
+    url: "https://www.meta.com/affiliates/ED1G655iA",
   },
   {
     image: "img/Banner/banner_image_1.webp",
@@ -12,21 +13,21 @@ export const bannerImagesList: DataImage[] = [
     url: "/games",
     contentId: "",
   },
-  {
-    image: "img/Banner/Exfilzone.webp",
-    altText: "Banner de videojuegos",
-    url: "https://www.meta.com/affiliates/5UwuzwgNo",
-    contentId: "",
+   {
+    image: "img/Banner/ghosttownbanner.webp",
+    altText: "Videojuego de realidad virtual llamado Ghost Town",
+    url: "https://www.meta.com/affiliates/6faeBg6Ke",
   },
   {
     image: "img/Banner/banner_image_2.webp",
     altText: "Banner de accesorios de realidad virtual",
     url: "https://www.meta.com/affiliates/DQCXmgkma",
   },
-
-   {
-    image: "img/Banner/bannerciv7.webp",
-    altText: "Videojuego de realidad virtual llamado CIVILIZATION",
-    url: "https://www.meta.com/affiliates/ED1G655iA",
+ {
+    image: "img/Banner/Exfilzone.webp",
+    altText: "Banner de videojuegos",
+    url: "https://www.meta.com/affiliates/5UwuzwgNo",
+    contentId: "",
   }
+   
 ];
