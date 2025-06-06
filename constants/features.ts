8350a9441a6be3aeea1785ac75c36e6a1aeab52a
DriDGG <yeray1060@gmail.com>
2025-06-06 12:27:24 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -1,15 +1,32 @@
 import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 
 export const featureGames: GameSchema[] = [
+    
     {
-    "name": "The Break-in",
-    "affiliate_link": "https://www.meta.com/affiliates/5UeKrV7ic",
-    "img_link": "./img/Games/499301272_2471704439860100_3033600562958149160_n.jpg",
+    "name": "Little Thief",
+    "affiliate_link": "https://www.meta.com/affiliates/EngnMGXvq",
+    "img_link": "./img/Games/473393827_1073842481262502_1368921964521548366_n.jpg",
     "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
   },
     {
+    "name": "Wordomi",
+    "affiliate_link": "https://www.meta.com/affiliates/CdLuD9SiE",
+    "img_link": "./img/Games/499303370_2070587570102093_8966402959895610440_n.webp",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+     {
+    "name": "The Phoenix Gene",
+    "affiliate_link": "https://www.meta.com/affiliates/AoxrxKikS",
+    "img_link": "./img/Games/38974638_551872451119311_3161064064341951833_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+     {
     "name": "Bad Bunny Bonanza",
     "affiliate_link": "https://www.meta.com/affiliates/AWAfZhXXD",
     "img_link": "./img/Games/499305529_1391268078742409_4176060152067807961_n.jpg",
@@ -18,26 +35,10 @@ export const featureGames: GameSchema[] = [
     "bhaptics": 0
   },
     {
-    "name": "Hard Bullet",
-    "affiliate_link": "https://www.meta.com/affiliates/929QN6foh",
-    "img_link": "./img/Games/75196230_865179729110613_8326642492619511638_n.jpg",
-    "yt_link": "https://youtu.be/S_DhA5EKoj4?si=v53ywHiQ5C9FSj3I",
-    "crossbuy": 0,
-    "bhaptics": 1
-  },
-    {
-        "name": "Into the radius 2",
-        "affiliate_link": "https://www.meta.com/es-es/experiences/into-the-radius-2/6605328856182465/?PID=101284762&cjevent=98482816367411f0810c00a60a18b8f7&utm_source=cj&utm_medium=affiliate&utm_campaign=creatoraffiliate&utm_parent=frl",
-        "img_link": "./img/Games/491819281_1220450749669486_3954912497666550359_n.jpg",
-        "yt_link": "https://youtu.be/EGwLNm3HVOQ?si=vUHvlwHmGswR5dJq",
-        "crossbuy": 0,
-        "bhaptics": 1
-    },
-    {
-    "name": "Surviving Mars: Pioneer",
-    "affiliate_link": "https://www.meta.com/affiliates/CRwnkMcAK",
-    "img_link": "./img/Games/491830915_1634861297163168_4285532260596147144_n.jpg",
-    "yt_link": "https://youtu.be/itxnMdIPEFI",
+    "name": "The Break-in",
+    "affiliate_link": "https://www.meta.com/affiliates/5UeKrV7ic",
+    "img_link": "./img/Games/499301272_2471704439860100_3033600562958149160_n.jpg",
+    "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
   }
