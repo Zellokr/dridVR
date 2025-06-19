7acf8bd71c67425d342aa8b174bb3700bbe8a938
DriDGG <yeray1060@gmail.com>
2025-06-19 12:05:49 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -1,33 +1,33 @@
 import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
+
+   {
+    image: "img/Banner/OVRDarkBanner.webp",
+    altText: "Videojuego de realidad virtual llamado OVRDark",
+    url: "https://www.meta.com/affiliates/6g3DmKuSf",
+  },
+   {
+    image: "img/Banner/banner_image_1.webp",
+    altText: "Banner de videojuegos",
+    url: "/games",
+    contentId: "",
+  },
    {
     image: "img/Banner/ZombieArmyBanner.jpg",
     altText: "Banner de videojuegos",
     url: "https://www.tkqlhce.com/click-101284762-15639798?url=https%3A%2F%2Fwww.meta.com%2Fexperiences%2F6133688003342021%2F&cjsku=6133688003342021",
     contentId: "",
   },
-  {
-    image: "img/Banner/banner_image_1.webp",
-    altText: "Banner de videojuegos",
-    url: "/games",
-    contentId: "",
+   {
+    image: "img/Banner/banner_image_2.webp",
+    altText: "Banner de accesorios de realidad virtual",
+    url: "https://www.meta.com/affiliates/DQCXmgkma",
   },
     {
     image: "img/Banner/IntotheradiusBanner.webp",
     altText: "Videojuego de realidad virtual llamado Into The Radius 2",
     url: "https://www.meta.com/affiliates/ED1G655iA",
-  },
-  
-  {
-    image: "img/Banner/banner_image_2.webp",
-    altText: "Banner de accesorios de realidad virtual",
-    url: "https://www.meta.com/affiliates/DQCXmgkma",
-  },
-  {
-    image: "img/Banner/ghosttownbanner.webp",
-    altText: "Videojuego de realidad virtual llamado Ghost Town",
-    url: "https://www.meta.com/affiliates/6faeBg6Ke",
   }
    
 ];
