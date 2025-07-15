e8f54b96ae6b1c368acea1e2eba17b12e8d4815a
DriDGG <yeray1060@gmail.com>
2025-07-15 11:57:40 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -1,34 +1,35 @@
 import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
-{
-    image: "img/Banner/VRacer-Hoverbike-Banner.webp",
-    altText: "Videojuego de realidad virtual llamado VRacer",
-    url: "https://www.meta.com/affiliates/G9hmEs2FU",
+
+    
+    
+    {
+    image: "img/Banner/RequisitionBanner.jpg",
+    altText: "Banner de videojuegos",
+    url: "https://www.meta.com/affiliates/FnRc5KEPq",
+    contentId: "",
   },
-  
    {
     image: "img/Banner/banner_image_1.webp",
     altText: "Banner de videojuegos",
     url: "/games",
     contentId: "",
   },
-    {
-    image: "img/Banner/OVRDarkBanner.webp",
-    altText: "Videojuego de realidad virtual llamado OVRDark",
-    url: "https://www.meta.com/affiliates/6g3DmKuSf",
+  {
+    image: "img/Banner/VRacer-Hoverbike-Banner.webp",
+    altText: "Videojuego de realidad virtual llamado VRacer",
+    url: "https://www.meta.com/affiliates/G9hmEs2FU",
   },
- 
    {
     image: "img/Banner/banner_image_2.webp",
     altText: "Banner de accesorios de realidad virtual",
     url: "https://www.meta.com/affiliates/DQCXmgkma",
   },
-    {
-    image: "img/Banner/ZombieArmyBanner.jpg",
-    altText: "Banner de videojuegos",
-    url: "https://www.tkqlhce.com/click-101284762-15639798?url=https%3A%2F%2Fwww.meta.com%2Fexperiences%2F6133688003342021%2F&cjsku=6133688003342021",
-    contentId: "",
-  }
+     {
+    image: "img/Banner/OVRDarkBanner.webp",
+    altText: "Videojuego de realidad virtual llamado OVRDark",
+    url: "https://www.meta.com/affiliates/6g3DmKuSf",
+  },
    
 ];
