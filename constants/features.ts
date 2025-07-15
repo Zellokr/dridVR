80e982d01ba011a44089a85db888c9743c86afed
DriDGG <yeray1060@gmail.com>
2025-07-15 11:51:52 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -2,42 +2,42 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 
 export const featureGames: GameSchema[] = [
 
+  {
+    "name": "Squishies - Roll them Home!",
+    "affiliate_link": "https://www.meta.com/affiliates/AnLmza1iq",
+    "img_link": "./img/Games/38982687_1979972669174326_811258919036079806_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+      {
+    "name": "Jungle Man",
+    "affiliate_link": "https://www.meta.com/affiliates/Ckv3UJDC1",
+    "img_link": "./img/Games/499330442_967505141976061_613969703676099374_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+ {
+    "name": "Requisition VR",
+    "affiliate_link": "https://www.anrdoezrs.net/click-101284762-15639798?url=https%3A%2F%2Fwww.meta.com%2Fexperiences%2F6637952719604708%2F&cjsku=6637952719604708",
+    "img_link": "./img/Games/473391820_2695014487555867_5994030469744577522_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+      {
+    "name": "Prison Boss Prohibition VR",
+    "affiliate_link": "https://www.meta.com/affiliates/6m6ZJhndD",
+    "img_link": "./img/Games/499627221_697042603236084_1369880695833966521_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
      {
     "name": "Bearly Escape",
     "affiliate_link": "https://www.meta.com/affiliates/5ineEiitJ",
     "img_link": "./img/Games/491831382_1412915676518706_1152898940958832296_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-    {
-    "name": "Cues",
-    "affiliate_link": "https://www.meta.com/affiliates/9uJnVtdtK",
-    "img_link": "./img/Games/39031631_590426196787195_5642446749436630898_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-  {
-    "name": "Motion Soccer PRO",
-    "affiliate_link": "https://www.meta.com/affiliates/CPyRu3CkA",
-    "img_link": "./img/Games/38982629_582120361466313_308609222389375327_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-     {
-    "name": "EDMUND PUZZLE AND THE MYSTERY OF THE SACRED RELICS",
-    "affiliate_link": "https://www.meta.com/affiliates/AqrnixSpM",
-    "img_link": "./img/Games/57570310_6477123942364183_7018038926051172481_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-    {
-    "name": "Neolithic Dawn",
-    "affiliate_link": "https://www.meta.com/affiliates/CA4Gda2Z8",
-    "img_link": "./img/Games/39031586_408298952226104_1060054367771282837_n.jpg",
     "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
