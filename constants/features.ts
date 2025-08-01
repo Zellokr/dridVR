1771c9ce252840422bd94fe700d023825615a548
DriDGG <yeray1060@gmail.com>
2025-08-01 14:36:11 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -3,45 +3,44 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 export const featureGames: GameSchema[] = [
 
    {
+    "name": "Sky Runner: Drone Hero",
+    "affiliate_link": "https://www.meta.com/affiliates/HzCFNVygn",
+    "img_link": "./img/Games/499298329_1234369754597914_8679612175364558418_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+  {
+    "name": "Besige VR",
+    "affiliate_link": "https://www.meta.com/affiliates/6bJsjbsSd",
+    "img_link": "./img/Games/38974507_956970849616267_7523863736471477206_n.jpg",
+    "yt_link": "0",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+  {
+    "name": "STACKAAR",
+    "affiliate_link": "https://www.meta.com/affiliates/Bu6cf9MHa",
+    "img_link": "./img/Games/491820648_747863781099090_8231748124262064470_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+   {
+    "name": "BEATABLE",
+    "affiliate_link": "https://www.meta.com/affiliates/Av8GEANXa",
+    "img_link": "./img/Games/507818746_2740884629435929_424663165203185797_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+ {
     "name": "99 DROPS",
     "affiliate_link": "https://www.meta.com/affiliates/6zrm2FJ4M",
     "img_link": "./img/Games/499295625_1923943391780416_2836023731105236100_n.jpg",
     "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
-  },
-  {
-    "name": "Elsewhere Electric",
-    "affiliate_link": "https://www.meta.com/affiliates/AyUT1A8ms",
-    "img_link": "./img/Games/499619836_722636813697848_8504768594959971390_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-  {
-    "name": "Squishies - Roll them Home!",
-    "affiliate_link": "https://www.meta.com/affiliates/AnLmza1iq",
-    "img_link": "./img/Games/38982687_1979972669174326_811258919036079806_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-      {
-    "name": "Jungle Man",
-    "affiliate_link": "https://www.meta.com/affiliates/Ckv3UJDC1",
-    "img_link": "./img/Games/499330442_967505141976061_613969703676099374_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
- {
-    "name": "Requisition VR",
-    "affiliate_link": "https://www.anrdoezrs.net/click-101284762-15639798?url=https%3A%2F%2Fwww.meta.com%2Fexperiences%2F6637952719604708%2F&cjsku=6637952719604708",
-    "img_link": "./img/Games/473391820_2695014487555867_5994030469744577522_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
   }
-   
   
 ];
