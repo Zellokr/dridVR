2fc0a034e7c1bfe49481aedb34c4f996d4d7d371
DriDGG <yeray1060@gmail.com>
2025-08-08 10:28:50 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -2,34 +2,34 @@ import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
 
-    
-    
-    {
-    image: "img/Banner/BesigeVR_Banner.webp",
-    altText: "Banner de videojuegos",
-    url: "https://www.meta.com/affiliates/6bJsjbsSd",
-    contentId: "",
+     {
+    image: "img/Banner/Copy of HiddenMemories_CoverImageWide.png",
+    altText: "Videojuego de realidad virtual llamado HiddenMemories",
+    url: "https://www.meta.com/affiliates/CM5A1Ron9",
   },
+  
    {
     image: "img/Banner/banner_image_1.webp",
     altText: "Banner de videojuegos",
     url: "/games",
     contentId: "",
   },
-  {
-    image: "img/Banner/VRacer-Hoverbike-Banner.webp",
-    altText: "Videojuego de realidad virtual llamado VRacer",
-    url: "https://www.meta.com/affiliates/G9hmEs2FU",
+      
+    {
+    image: "img/Banner/BesigeVR_Banner.webp",
+    altText: "Banner de videojuegos",
+    url: "https://www.meta.com/affiliates/6bJsjbsSd",
+    contentId: "",
   },
+ 
    {
     image: "img/Banner/banner_image_2.webp",
     altText: "Banner de accesorios de realidad virtual",
     url: "https://www.meta.com/affiliates/DQCXmgkma",
   },
-     {
-    image: "img/Banner/OVRDarkBanner.webp",
-    altText: "Videojuego de realidad virtual llamado OVRDark",
-    url: "https://www.meta.com/affiliates/6g3DmKuSf",
-  },
-   
+    {
+    image: "img/Banner/VRacer-Hoverbike-Banner.webp",
+    altText: "Videojuego de realidad virtual llamado VRacer",
+    url: "https://www.meta.com/affiliates/G9hmEs2FU",
+  }
 ];
