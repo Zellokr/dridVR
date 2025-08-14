74a0d6c0b4ce0118b85667d393f2d453749d50e6
DriDGG <yeray1060@gmail.com>
2025-08-14 20:47:56 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -1,35 +1,33 @@
 import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
-
-     {
-    image: "img/Banner/hiddenmemories.webp",
-    altText: "Videojuego de realidad virtual llamado HiddenMemories",
-    url: "https://www.meta.com/affiliates/CM5A1Ron9",
+{
+    image: "img/Banner/WorldWarZ.webp",
+    altText: "Videojuego de realidad virtual llamado World War Z VR",
+    url: "https://www.meta.com/affiliates/B1yfXkQF8",
   },
-  
    {
     image: "img/Banner/banner_image_1.webp",
     altText: "Banner de videojuegos",
     url: "/games",
     contentId: "",
   },
-      
-    {
-    image: "img/Banner/BesigeVR_Banner.webp",
-    altText: "Banner de videojuegos",
-    url: "https://www.meta.com/affiliates/6bJsjbsSd",
-    contentId: "",
+{
+    image: "img/Banner/hiddenmemories.webp",
+    altText: "Videojuego de realidad virtual llamado HiddenMemories",
+    url: "https://www.meta.com/affiliates/CM5A1Ron9",
   },
+    
  
    {
     image: "img/Banner/banner_image_2.webp",
     altText: "Banner de accesorios de realidad virtual",
     url: "https://www.meta.com/affiliates/DQCXmgkma",
   },
-    {
-    image: "img/Banner/VRacer-Hoverbike-Banner.webp",
-    altText: "Videojuego de realidad virtual llamado VRacer",
-    url: "https://www.meta.com/affiliates/G9hmEs2FU",
+   {
+    image: "img/Banner/BesigeVR_Banner.webp",
+    altText: "Banner de videojuegos",
+    url: "https://www.meta.com/affiliates/6bJsjbsSd",
+    contentId: "",
   }
 ];
