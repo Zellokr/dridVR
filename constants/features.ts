a08738e19ba088e6ba02da83f9351dbaf81d8105
DriDGG <yeray1060@gmail.com>
2025-08-29 12:17:29 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -1,7 +1,24 @@
 import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 
 export const featureGames: GameSchema[] = [
+
+    {
+    "name": "Metacity Patrol",
+    "affiliate_link": "https://www.meta.com/affiliates/BP6G934iS",
+    "img_link": "./img/Games/499617786_2236900290072001_7393520953640645694_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
 {
+    "name": "IRON GUARD: Salvation",
+    "affiliate_link": "https://www.meta.com/affiliates/6ohXJ5nRh",
+    "img_link": "./img/Games/491821849_1537754117189646_7015956685767300346_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+    {
     "name": "Grit and Valor - 1949",
     "affiliate_link": "https://www.meta.com/affiliates/DEfyuTMMX",
     "img_link": "./img/Games/499304034_1410506846827506_3899759777801896798_n.jpg",
@@ -22,22 +39,6 @@ export const featureGames: GameSchema[] = [
     "affiliate_link": "https://www.meta.com/affiliates/6mgUwAkGC",
     "img_link": "./img/Games/499618000_754894260354367_347896483049409463_n.jpg",
     "yt_link": "https://youtu.be/suwlcqE0qww?si=WJa_G9hn4Rettpw5",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-    {
-    "name": "World War Z",
-    "affiliate_link": "https://www.meta.com/affiliates/B1yfXkQF8",
-    "img_link": "./img/Games/473392010_2428237764241646_3393249897285510383_n.jpg",
-    "yt_link": "https://youtu.be/sJ_efFmlmmo?si=z0W6cEbwD9oc6zL8",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-   {
-    "name": "Can You Do It - VR Experience",
-    "affiliate_link": "https://www.meta.com/affiliates/9WMKVZwUS",
-    "img_link": "./img/Games/39031462_765929624052423_5560774879576763627_n.jpg",
-    "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
   }
