51254c12dbc26733b65208d7c45e81a86c5d9ba6
DriDGG <yeray1060@gmail.com>
2025-09-12 17:23:39 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -1,11 +1,13 @@
 import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
-{
-    image: "img/Banner/boxingunderdog.webp",
-    altText: "Videojuego de realidad virtual llamado Boxing Underdog VR",
-    url: "https://www.meta.com/affiliates/Ay8NpV9gK",
+
+    {
+    image: "img/Banner/ofliesandrainbanner.webp",
+    altText: "Videojuego de realidad virtual llamado World War Z VR",
+    url: "https://www.meta.com/affiliates/B1yfXkQF8",
   },
+   
    {
     image: "img/Banner/banner_image_1.webp",
     altText: "Banner de videojuegos",
@@ -13,12 +15,11 @@ export const bannerImagesList: DataImage[] = [
     contentId: "",
   },
 {
-    image: "img/Banner/worldwarzvr.webp",
-    altText: "Videojuego de realidad virtual llamado World War Z VR",
-    url: "https://www.meta.com/affiliates/B1yfXkQF8",
+  {
+    image: "img/Banner/boxingunderdog.webp",
+    altText: "Videojuego de realidad virtual llamado Boxing Underdog VR",
+    url: "https://www.meta.com/affiliates/Ay8NpV9gK",
   },
-    
- 
    {
     image: "img/Banner/banner_image_2.webp",
     altText: "Banner de accesorios de realidad virtual",
