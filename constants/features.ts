1a148ffc5652a57699aef98f59c6f6f2ede1ab8a
DriDGG <yeray1060@gmail.com>
2025-09-12 19:08:35 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -3,6 +3,30 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 export const featureGames: GameSchema[] = [
 
      {
+    "name": "Table Troopers",
+    "affiliate_link": "https://www.meta.com/affiliates/B9NBusG8V",
+    "img_link": "./img/Games/491823296_4165423037052931_2427186324510205780_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+      {
+    "name": "Pencil - Learn to Draw ",
+    "affiliate_link": "https://www.meta.com/affiliates/GEgW6WhNk",
+    "img_link": "./img/Games/491820001_1039458434741209_3286019584445384794_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+     {
+    "name": "Successor",
+    "affiliate_link": "https://www.meta.com/affiliates/A5i8Du4MU",
+    "img_link": "./img/Games/491820382_1272984167401454_5799731337705662969_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+     {
     "name": "Unloop",
     "affiliate_link": "https://www.meta.com/affiliates/AhJwaPcq8",
     "img_link": "./img/Games/499300897_1208389670955554_3564909399670881390_n.jpg",
@@ -23,22 +47,6 @@ export const featureGames: GameSchema[] = [
     "affiliate_link": "https://www.meta.com/affiliates/787VMiPiF",
     "img_link": "./img/Games/499297259_2106964126482010_9186249825901896897_n.jpg",
     "yt_link": "https://youtu.be/9m0u4JrDrvU",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-    {
-    "name": "Metacity Patrol",
-    "affiliate_link": "https://www.meta.com/affiliates/BP6G934iS",
-    "img_link": "./img/Games/499617786_2236900290072001_7393520953640645694_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-{
-    "name": "IRON GUARD: Salvation",
-    "affiliate_link": "https://www.meta.com/affiliates/6ohXJ5nRh",
-    "img_link": "./img/Games/491821849_1537754117189646_7015956685767300346_n.jpg",
-    "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
   }
