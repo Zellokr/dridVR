6e2c986da8898500e36b06acfa70f20d604f1ebc
DriDGG <yeray1060@gmail.com>
2025-09-26 10:43:42 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -2,7 +2,14 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 
 export const featureGames: GameSchema[] = [
 
-     
+     {
+    "name": "Chrono Hunter",
+    "affiliate_link": "https://www.meta.com/affiliates/EtNkoSxCC",
+    "img_link": "./img/Games/491821799_690710280657994_2641653685966979585_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  }, 
      {
     "name": "Titanic VR",
     "affiliate_link": "https://www.meta.com/affiliates/YNPPwnmHT",
@@ -31,22 +38,6 @@ export const featureGames: GameSchema[] = [
     "name": "Successor",
     "affiliate_link": "https://www.meta.com/affiliates/A5i8Du4MU",
     "img_link": "./img/Games/491820382_1272984167401454_5799731337705662969_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-     {
-    "name": "Unloop",
-    "affiliate_link": "https://www.meta.com/affiliates/AhJwaPcq8",
-    "img_link": "./img/Games/499300897_1208389670955554_3564909399670881390_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-    {
-    "name": "Slap Fighter",
-    "affiliate_link": "https://www.meta.com/affiliates/J1rt4pecN",
-    "img_link": "./img/Games/473393543_1310943550212341_7603591577712594748_n.jpg",
     "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
