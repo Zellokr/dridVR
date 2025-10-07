6e55990d2677bf921f9c0f146297e4a175311161
DriDGG <yeray1060@gmail.com>
2025-10-07 20:16:23 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -2,26 +2,32 @@ import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
 
-    {
-    image: "img/Banner/ofliesandrainbanner.webp",
-    altText: "Videojuego de realidad virtual llamado OF Lies and Rain",
-    url: "https://www.meta.com/affiliates/A2XP55jDS",
+    
+   {
+    image: "img/Banner/StarWarsBanner.webp",
+    altText: "Videojuego de realidad virtual llamado Star Wars Beyond Victory",
+    url: "https://www.meta.com/affiliates/7SAA35ta3",
   },
-   
    {
     image: "img/Banner/banner_image_1.webp",
     altText: "Banner de videojuegos",
     url: "/games",
     contentId: "",
   },
-  {
-    image: "img/Banner/boxingunderdog.webp",
-    altText: "Videojuego de realidad virtual llamado Boxing Underdog VR",
-    url: "https://www.meta.com/affiliates/Ay8NpV9gK",
+    {
+    image: "img/Banner/ofliesandrainbanner.webp",
+    altText: "Videojuego de realidad virtual llamado OF Lies and Rain",
+    url: "https://www.meta.com/affiliates/A2XP55jDS",
   },
+  
    {
     image: "img/Banner/banner_image_2.webp",
     altText: "Banner de accesorios de realidad virtual",
     url: "https://www.meta.com/affiliates/DQCXmgkma",
+  },
+    {
+    image: "img/Banner/boxingunderdog.webp",
+    altText: "Videojuego de realidad virtual llamado Boxing Underdog VR",
+    url: "https://www.meta.com/affiliates/Ay8NpV9gK",
   }
 ];
