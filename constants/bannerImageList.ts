5a33ebbcbbbe341f3d292e8ba36626c08d6e69a6
DriDGG <yeray1060@gmail.com>
2025-10-09 20:56:40 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -14,10 +14,10 @@ export const bannerImagesList: DataImage[] = [
     url: "/games",
     contentId: "",
   },
-    {
-    image: "img/Banner/ofliesandrainbanner.webp",
-    altText: "Videojuego de realidad virtual llamado OF Lies and Rain",
-    url: "https://www.meta.com/affiliates/A2XP55jDS",
+   {
+    image: "img/Banner/MemoreumBanner.webp",
+    altText: "Videojuego de realidad virtual llamado MEMOREUM",
+    url: "https://www.meta.com/affiliates/HPvJdnRMK",
   },
   
    {
@@ -26,8 +26,8 @@ export const bannerImagesList: DataImage[] = [
     url: "https://www.meta.com/affiliates/DQCXmgkma",
   },
     {
-    image: "img/Banner/boxingunderdog.webp",
-    altText: "Videojuego de realidad virtual llamado Boxing Underdog VR",
-    url: "https://www.meta.com/affiliates/Ay8NpV9gK",
+    image: "img/Banner/ofliesandrainbanner.webp",
+    altText: "Videojuego de realidad virtual llamado OF Lies and Rain",
+    url: "https://www.meta.com/affiliates/A2XP55jDS",
   }
 ];
