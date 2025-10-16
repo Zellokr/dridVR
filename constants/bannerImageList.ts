654ce269f78984bfa33457bbd96b44a21ac68a80
DriDGG <yeray1060@gmail.com>
2025-10-16 20:24:10 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -2,11 +2,10 @@ import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
 
-    
-   {
-    image: "img/Banner/StarWarsBanner.webp",
-    altText: "Videojuego de realidad virtual llamado Star Wars Beyond Victory",
-    url: "https://www.meta.com/affiliates/7SAA35ta3",
+     {
+    image: "img/Banner/ReachBanner.webp",
+    altText: "Videojuego de realidad virtual llamado Reach",
+    url: "https://www.meta.com/affiliates/BdkWUhqGQ",
   },
    {
     image: "img/Banner/banner_image_1.webp",
@@ -15,19 +14,19 @@ export const bannerImagesList: DataImage[] = [
     contentId: "",
   },
    {
-    image: "img/Banner/MemoreumBanner.webp",
-    altText: "Videojuego de realidad virtual llamado MEMOREUM",
-    url: "https://www.meta.com/affiliates/HPvJdnRMK",
+    image: "img/Banner/StarWarsBanner.webp",
+    altText: "Videojuego de realidad virtual llamado Star Wars Beyond Victory",
+    url: "https://www.meta.com/affiliates/7SAA35ta3",
   },
-  
    {
     image: "img/Banner/banner_image_2.webp",
     altText: "Banner de accesorios de realidad virtual",
     url: "https://www.meta.com/affiliates/DQCXmgkma",
   },
-    {
-    image: "img/Banner/ofliesandrainbanner.webp",
-    altText: "Videojuego de realidad virtual llamado OF Lies and Rain",
-    url: "https://www.meta.com/affiliates/A2XP55jDS",
+   {
+    image: "img/Banner/MemoreumBanner.webp",
+    altText: "Videojuego de realidad virtual llamado MEMOREUM",
+    url: "https://www.meta.com/affiliates/HPvJdnRMK",
   }
+  
 ];
