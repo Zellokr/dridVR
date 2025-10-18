4bc41efb9c98f20aab2bcaf05b0331b2f55ec0fa
DriDGG <yeray1060@gmail.com>
2025-10-18 14:47:38 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -2,7 +2,14 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 
 export const featureGames: GameSchema[] = [
 
-
+{
+    "name": "VEX MAGE",
+    "affiliate_link": "https://www.meta.com/affiliates/CJwgvEzMY",
+    "img_link": "./img/Games/491822581_1079316964363502_2190154169289756009_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
       {
     "name": "Reach",
     "affiliate_link": "https://www.meta.com/affiliates/BdkWUhqGQ",
@@ -31,14 +38,6 @@ export const featureGames: GameSchema[] = [
     "name": "Newton's Playground : Physics Sandbox",
     "affiliate_link": "https://www.meta.com/affiliates/TQB8kuC7G",
     "img_link": "./img/Games/491827799_1553162825668552_7199299748654636922_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-     {
-    "name": "Chrono Hunter",
-    "affiliate_link": "https://www.meta.com/affiliates/EtNkoSxCC",
-    "img_link": "./img/Games/491821799_690710280657994_2641653685966979585_n.jpg",
     "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
