3c9560c2a52b85a0c839c10a8410dcce15b3b052
DriDGG <yeray1060@gmail.com>
2025-10-31 14:49:12 +0000

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -2,6 +2,30 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 
 export const featureGames: GameSchema[] = [
 
+    {
+    "name": "Arken Age",
+    "affiliate_link": "https://www.meta.com/affiliates/AtvwqtGLj",
+    "img_link": "./img/Games/491831273_2072747453216770_6312138702007251495_n.jpg",
+    "yt_link": "https://youtu.be/m2YOLwGaZ3g?si=eIGqUbZHqPsped3a",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+     {
+    "name": "A Long Survive",
+    "affiliate_link": "https://www.meta.com/affiliates/7XCagx2YH",
+    "img_link": "./img/Games/499296890_1945823209539050_2237746196065130574_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+    {
+    "name": "House of Golf VR",
+    "affiliate_link": "https://www.meta.com/affiliates/7UjV7oZWH",
+    "img_link": "./img/Games/499310646_1840856126503288_2680082295238938645_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
 {
     "name": "VEX MAGE",
     "affiliate_link": "https://www.meta.com/affiliates/CJwgvEzMY",
@@ -15,30 +39,6 @@ export const featureGames: GameSchema[] = [
     "affiliate_link": "https://www.meta.com/affiliates/BdkWUhqGQ",
     "img_link": "/img/Games/499312463_712386211172039_4348263024996989382_n.jpg",
     "yt_link": "https://youtu.be/47ph_cdEzNE?si=sRtuwvPLQhwTPSHy",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-     {
-    "name": "MEMOREUM",
-    "affiliate_link": "https://www.meta.com/affiliates/HPvJdnRMK",
-    "img_link": "/img/Games/473393879_660964106460783_1669645472851690461_n.jpg",
-    "yt_link": "https://youtu.be/T2IG7Zun7kQ?si=uvXgxjXyQG8sBeTS",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-    {
-    "name": "Star Wars: Beyond Victory",
-    "affiliate_link": "https://www.meta.com/affiliates/7SAA35ta3",
-    "img_link": "/img/Games/499299745_3074995456008715_5855507417964469643_n.jpg",
-    "yt_link": "https://youtu.be/aPkaCmcJ50E?si=4ImfHAiNiyguIT-Q",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-    {
-    "name": "Newton's Playground : Physics Sandbox",
-    "affiliate_link": "https://www.meta.com/affiliates/TQB8kuC7G",
-    "img_link": "/img/Games/491827799_1553162825668552_7199299748654636922_n.jpg",
-    "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
   }
