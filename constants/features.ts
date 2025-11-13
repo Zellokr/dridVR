8f3d4bb4d2613ef34be84bd736ccc022b57b6fd3
DriDGG <yeray1060@gmail.com>
2025-11-13 20:34:15 +0000

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -3,25 +3,25 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 export const featureGames: GameSchema[] = [
 
     {
-    "name": "Arken Age",
-    "affiliate_link": "https://www.meta.com/affiliates/AtvwqtGLj",
-    "img_link": "./img/Games/491831273_2072747453216770_6312138702007251495_n.jpg",
-    "yt_link": "https://youtu.be/m2YOLwGaZ3g?si=eIGqUbZHqPsped3a",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-     {
-    "name": "A Long Survive",
-    "affiliate_link": "https://www.meta.com/affiliates/7XCagx2YH",
-    "img_link": "./img/Games/499296890_1945823209539050_2237746196065130574_n.jpg",
+    "name": "Glassbreakers",
+    "affiliate_link": "https://www.meta.com/affiliates/ADjn1c4dy",
+    "img_link": "./img/Games/499617729_662176040090161_5560587289857672521_n.jpg",
     "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
   },
     {
-    "name": "House of Golf VR",
-    "affiliate_link": "https://www.meta.com/affiliates/7UjV7oZWH",
-    "img_link": "./img/Games/499310646_1840856126503288_2680082295238938645_n.jpg",
+    "name": "TRACKED: Shoot to Survive",
+    "affiliate_link": "https://www.meta.com/affiliates/7mZsXHr8K",
+    "img_link": "./img/Games/491821782_771258645931025_7949299798195120549_n.jpg",
+    "yt_link": "https://youtu.be/U3MoiLJ6E74",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+    {
+    "name": "Vampire Survivors VR",
+    "affiliate_link": "https://www.meta.com/affiliates/ByEsF76A5",
+    "img_link": "./img/Games/499304305_1327599448750109_2787820878676889286_n.jpg",
     "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
