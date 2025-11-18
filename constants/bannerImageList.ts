a446df27596967e258c655a1362863cd490ce6a3
DriDGG <yeray1060@gmail.com>
2025-11-18 13:30:29 +0000

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -2,7 +2,12 @@ import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
 
-     
+
+        {
+    image: "/img/Banner/Quest3SBlackFriday.webp",
+    altText: "Banner de accesorios de realidad virtual",
+    url: "https://www.meta.com/affiliates/AbzRrTmFY",
+  },
       {
     image: "/img/Banner/ForefrontBanner.webp",
     altText: "Videojuego de realidad virtual llamado Forefront",
@@ -20,12 +25,7 @@ export const bannerImagesList: DataImage[] = [
     altText: "Videojuego de realidad virtual llamado Reach",
     url: "https://www.meta.com/affiliates/BdkWUhqGQ",
   },
-   
-   {
-    image: "/img/Banner/banner_image_2.webp",
-    altText: "Banner de accesorios de realidad virtual",
-    url: "https://www.meta.com/affiliates/DQCXmgkma",
-  },
+  
   {
     image: "/img/Banner/StarWarsBanner.webp",
     altText: "Videojuego de realidad virtual llamado Star Wars Beyond Victory",
