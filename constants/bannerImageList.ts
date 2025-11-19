4c8832639a5d236fc0068f470729cc79059833ca
DriDGG <yeray1060@gmail.com>
2025-11-19 09:59:06 +0000

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -2,7 +2,12 @@ import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
 
-
+ {
+    image: "/img/Banner/BlackFridayBanner.webp",
+    altText: "Banner de videojuegos",
+    url: "/games",
+    contentId: "",
+  },
         {
     image: "/img/Banner/Quest3SBlackFriday.webp",
     altText: "Banner de accesorios de realidad virtual",
@@ -14,12 +19,6 @@ export const bannerImagesList: DataImage[] = [
     url: "https://www.meta.com/affiliates/7RdDupYuv",
   },
      
-   {
-    image: "/img/Banner/banner_image_1.webp",
-    altText: "Banner de videojuegos",
-    url: "/games",
-    contentId: "",
-  },
      {
     image: "/img/Banner/ReachBanner.webp",
     altText: "Videojuego de realidad virtual llamado Reach",
