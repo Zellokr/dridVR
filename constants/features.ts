f1b6b42bd95acd2517199b5ca9af95f4780c05f8
DriDGG <yeray1060@gmail.com>
2025-11-26 11:48:14 +0000

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -3,10 +3,18 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 export const featureGames: GameSchema[] = [
 
      {
+    "name": "Aim Assault",
+    "affiliate_link": "https://www.meta.com/affiliates/N7FzPETcW",
+    "img_link": "./img/Games/491825034_1134829201776127_3401037682143489321_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+     {
     "name": "Demeo x Dungeons & Dragons: Battlemarked",
     "affiliate_link": "https://www.meta.com/affiliates/BQKF62DeV",
     "img_link": "./img/Games/499305027_653730900666136_5648357199524809106_n.jpg",
-    "yt_link": "",
+    "yt_link": "https://youtu.be/EOL8afY_6hU",
     "crossbuy": 0,
     "bhaptics": 0
   },
@@ -30,14 +38,6 @@ export const featureGames: GameSchema[] = [
     "name": "Vampire Survivors VR",
     "affiliate_link": "https://www.meta.com/affiliates/ByEsF76A5",
     "img_link": "./img/Games/499304305_1327599448750109_2787820878676889286_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-{
-    "name": "Syberia",
-    "affiliate_link": "https://www.meta.com/affiliates/4RaKH8RvPR",
-    "img_link": "./img/Games/491820382_1822667978462767_6089766565430026358_n.jpg",
     "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
