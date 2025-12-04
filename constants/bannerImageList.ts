9433ece331aabe587f6786a96963f4163061d3b7
DriDGG <yeray1060@gmail.com>
2025-12-04 19:59:08 +0000

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -8,28 +8,27 @@ export const bannerImagesList: DataImage[] = [
     url: "/games",
     contentId: "",
   },
-    {
-    image: "/img/Banner/DemeoDungeonAndDragonBanner.webp",
-    altText: "Videojuego de realidad virtual llamado Demeo",
-    url: "https://www.meta.com/affiliates/BQKF62DeV",
+  {
+    image: "/img/Banner/ThiefBanner.webp",
+    altText: "Videojuego de realidad virtual llamado Star Wars Beyond Victory",
+    url: "https://www.meta.com/affiliates/6VMAU4PRV",
   },
+    
         {
     image: "/img/Banner/Quest3SBlackFriday.webp",
     altText: "Banner de accesorios de realidad virtual",
     url: "https://www.meta.com/affiliates/AbzRrTmFY",
   },
-   
+   {
+    image: "/img/Banner/DemeoDungeonAndDragonBanner.webp",
+    altText: "Videojuego de realidad virtual llamado Demeo",
+    url: "https://www.meta.com/affiliates/BQKF62DeV",
+  },
      
      {
     image: "/img/Banner/ReachBanner.webp",
     altText: "Videojuego de realidad virtual llamado Reach",
     url: "https://www.meta.com/affiliates/BdkWUhqGQ",
-  },
-  
-  {
-    image: "/img/Banner/StarWarsBanner.webp",
-    altText: "Videojuego de realidad virtual llamado Star Wars Beyond Victory",
-    url: "https://www.meta.com/affiliates/7SAA35ta3",
   }
 
 ];
