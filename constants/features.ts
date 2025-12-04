a31e33b1f7392864dab879843253911c5b63ff48
DriDGG <yeray1060@gmail.com>
2025-12-04 18:10:54 +0000

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -3,6 +3,14 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 export const featureGames: GameSchema[] = [
 
      {
+    "name": "Deadly Delivery",
+    "affiliate_link": "https://www.meta.com/affiliates/Ky2eBYcT9",
+    "img_link": "./img/Games/499303788_790652400613514_7573602946737861350_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+     {
     "name": "Aim Assault",
     "affiliate_link": "https://www.meta.com/affiliates/N7FzPETcW",
     "img_link": "./img/Games/491825034_1134829201776127_3401037682143489321_n.jpg",
@@ -31,14 +39,6 @@ export const featureGames: GameSchema[] = [
     "affiliate_link": "https://www.meta.com/affiliates/7mZsXHr8K",
     "img_link": "./img/Games/491821782_771258645931025_7949299798195120549_n.jpg",
     "yt_link": "https://youtu.be/U3MoiLJ6E74",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-    {
-    "name": "Vampire Survivors VR",
-    "affiliate_link": "https://www.meta.com/affiliates/ByEsF76A5",
-    "img_link": "./img/Games/499304305_1327599448750109_2787820878676889286_n.jpg",
-    "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
   }
