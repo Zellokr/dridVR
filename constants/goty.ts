ac6fdc22525535edeb0a8d30942314c7de7778e8
Kristian <kristianmartinezgarcia@gmail.com>
2025-12-26 14:21:39 +0000

    feat: add GOTY games data and display them on a new games page with SEO metadata and filtering.

---
+++ constants/goty.ts
@@ -0,0 +1,84 @@
+import type { SchemaGame } from "~/utils/types";
+
+export const gotyGames: SchemaGame[] = [
+  {
+    "name": "Star Wars: Beyond Victory",
+    "affiliate_link": "https://www.meta.com/affiliates/7SAA35ta3",
+    "img_link": "./img/Games/499299745_3074995456008715_5855507417964469643_n.jpg",
+    "yt_link": "https://youtu.be/aPkaCmcJ50E?si=NCINHtZFHwzMoVUs",
+    "crossbuy": false,
+    "bhaptics": false
+  },
+  {
+    "name": "Marvel's Deadpool VR",
+    "affiliate_link": "https://www.meta.com/affiliates/deadpool",
+    "img_link": "./img/Games/499312357_565944479572802_6463433371077248263_n.jpg",
+    "yt_link": "",
+    "crossbuy": false,
+    "bhaptics": false
+  },
+  {
+    "name": "Of Lies and Rain",
+    "affiliate_link": "https://www.meta.com/affiliates/A2XP55jDS",
+    "img_link": "./img/Games/473393575_973835214128727_5629459935029185258_n.jpg",
+    "yt_link": "https://youtu.be/qRGr4A1t8NI",
+    "crossbuy": false,
+    "bhaptics": false
+  },
+  {
+    "name": "Arken Age",
+    "affiliate_link": "https://www.meta.com/affiliates/AtvwqtGLj",
+    "img_link": "./img/Games/491831273_2072747453216770_6312138702007251495_n.jpg",
+    "yt_link": "https://youtu.be/m2YOLwGaZ3g?si=eIGqUbZHqPsped3a",
+    "crossbuy": false,
+    "bhaptics": false
+  },
+  {
+    "name": "MEMOREUM",
+    "affiliate_link": "https://www.meta.com/affiliates/HPvJdnRMK",
+    "img_link": "./img/Games/473393879_660964106460783_1669645472851690461_n.jpg",
+    "yt_link": "https://youtu.be/T2IG7Zun7kQ?si=uvXgxjXyQG8sBeTS",
+    "crossbuy": false,
+    "bhaptics": false
+  },
+  {
+    "name": "Forefront",
+    "affiliate_link": "https://www.meta.com/affiliates/forefront",
+    "img_link": "./img/Games/473393887_2470831469962675_6195762742504749911_n.jpg",
+    "yt_link": "",
+    "crossbuy": false,
+    "bhaptics": false
+  },
+  {
+    "name": "Ghost Town",
+    "affiliate_link": "https://www.meta.com/affiliates/6faeBg6Ke",
+    "img_link": "./img/Games/473391899_1346443866774466_5902162057010451886_n.jpg",
+    "yt_link": "https://youtu.be/v7cnPIsCXSA?si=lKwjjgp5Jbl3OSvJ",
+    "crossbuy": false,
+    "bhaptics": false
+  },
+  {
+    "name": "Demeo x Dungeons & Dragons Battlemarked",
+    "affiliate_link": "https://www.ftjcfx.com/click-101284762-15639798?url=https%3A%2F%2Fwww.meta.com%2Fexperiences%2F3634830803298285%2F&cjsku=3634830803298285&widgetid=67409c6d6e73e5d8cacefdc1",
+    "img_link": "./img/Games/499305027_653730900666136_5648357199524809106_n.jpg",
+    "yt_link": "",
+    "crossbuy": true,
+    "bhaptics": false
+  },
+  {
+    "name": "Reach",
+    "affiliate_link": "https://www.meta.com/affiliates/BdkWUhqGQ",
+    "img_link": "./img/Games/499312463_712386211172039_4348263024996989382_n.jpg",
+    "yt_link": "https://youtu.be/47ph_cdEzNE?si=sRtuwvPLQhwTPSHy",
+    "crossbuy": false,
+    "bhaptics": false
+  },
+  {
+    "name": "Thief VR - Legacy of Shadow",
+    "affiliate_link": "https://www.meta.com/affiliates/thief",
+    "img_link": "./img/Games/499332272_1073948757937270_7808984192494514144_n.jpg",
+    "yt_link": "",
+    "crossbuy": false,
+    "bhaptics": false
+  }
+];
