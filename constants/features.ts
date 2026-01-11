91cc777531a22afac1127f60acc15f753f6d3cf7
DriDGG <yeray1060@gmail.com>
2026-01-11 12:39:10 +0000

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -3,6 +3,22 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 export const featureGames: GameSchema[] = [
 
    {
+    "name": "Dixotomia",
+    "affiliate_link": "https://www.meta.com/affiliates/VEdDybCLp",
+    "img_link": "./img/Games/499324083_693562293477518_7730284112963869333_n.webp",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+    {
+    "name": "Groovy Labs",
+    "affiliate_link": "https://www.meta.com/affiliates/DZwUyiq6F",
+    "img_link": "./img/Games/491821115_1876706436573337_174493975721684781_n.webp",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+   {
     "name": "Walk the Plank",
     "affiliate_link": "https://www.meta.com/affiliates/CzXdpa1dQ",
     "img_link": "./img/Games/499622214_1912947135965986_1550361271224524682_n.webp",
@@ -23,22 +39,6 @@ export const featureGames: GameSchema[] = [
     "affiliate_link": "https://www.meta.com/affiliates/T8kNmGzGZ",
     "img_link": "./img/Games/491824741_1680680859322823_8499136835362803385_n.webp",
     "yt_link": "https://youtu.be/0hFDEEiK9so?si=SIOITiXyz0gfw9wE",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-    {
-    "name": "Street Gods",
-    "affiliate_link": "https://www.meta.com/affiliates/7TMcwN8xv",
-    "img_link": "./img/Games/491823746_2321496548310639_5332265616824231967_n.webp",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-    {
-    "name": "True Surf",
-    "affiliate_link": "https://www.meta.com/affiliates/DWfXnYMhT",
-    "img_link": "./img/Games/499618277_653478137457821_5458112418876366809_n.webp",
-    "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
   }
