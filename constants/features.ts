4cec4fcce8607da08657745572affb3ac7f34b56
DriDGG <yeray1060@gmail.com>
2026-04-06 22:55:33 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -3,41 +3,41 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 export const featureGames: GameSchema[] = [
 
    {
+    "name": "The Boys",
+    "affiliate_link": "https://www.meta.com/affiliates/Hyx6QCeSn",
+    "img_link": "./img/Games/491822723_4256648827994955_2985533148640339611_n.webp",
+    "yt_link": "https://youtu.be/nR2x2LTrArg?si=wIPLb0PnKJEJ-AWW",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+    {
+    "name": "Star Trek: Infection",
+    "affiliate_link": "https://www.meta.com/affiliates/BGHs7pzep",
+    "img_link": "./img/Games/491828899_24698915326393822_4129578363663631155_n.webp",
+    "yt_link": "https://youtu.be/YGhbtI3oaqY?si=ccNTewgcrDw5-rH4",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+   {
+    "name": "Beat the Beats",
+    "affiliate_link": "https://www.meta.com/affiliates/7UHwub3Lp",
+    "img_link": "./img/Games/38974887_903317764512651_4367060651741261931_n.webp",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+   {
+    "name": "Racket Pinball",
+    "affiliate_link": "https://www.meta.com/affiliates/8TXALubf9",
+    "img_link": "./img/Games/491824328_1336956178467458_2886774562409944223_n.webp",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+   {
     "name": "Cursed Echoes",
     "affiliate_link": "https://www.meta.com/affiliates/8TdmUjrRs",
     "img_link": "./img/Games/491819983_1406065877290819_3050450984097403096_n.webp",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-   {
-    "name": "Unseen Diplomacy 2",
-    "affiliate_link": "https://www.meta.com/affiliates/74vPLtkjD",
-    "img_link": "./img/Games/499296194_1502846007361603_5702462798731013770_n.webp",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-   {
-    "name": "Fight Fit VR",
-    "affiliate_link": "https://www.meta.com/affiliates/7i4j8vKrK",
-    "img_link": "./img/Games/39031319_1611631739761307_2491182172697861727_n.webp",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-     {
-    "name": "RAGER",
-    "affiliate_link": "https://www.meta.com/affiliates/8KBEiAStU",
-    "img_link": "./img/Games/499307931_1611900226497538_8787896665086869012_n.webp",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-   {
-    "name": "Le Dino Labo",
-    "affiliate_link": "https://www.meta.com/affiliates/Aj25Jx3fW",
-    "img_link": "./img/Games/491825462_852346813869199_3496863667959873628_n.webp",
     "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
