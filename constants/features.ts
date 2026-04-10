2524e8b34fd97f35fd27afb649477f99fbb61329
DriDGG <yeray1060@gmail.com>
2026-04-10 15:21:54 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -2,6 +2,22 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 
 export const featureGames: GameSchema[] = [
 
+    {
+    "name": "WRATH: Aeon of Ruin VR - Brutal Edition",
+    "affiliate_link": "https://www.meta.com/affiliates/8DmRM2WLP",
+    "img_link": "./img/Games/499620669_1803967224266511_3383253472074138301_n.webp",
+    "yt_link": "https://youtu.be/huj0OJhlD-E?si=3nOTuYo5HHyuZ9Oe",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+   {
+    "name": "Flag Games",
+    "affiliate_link": "https://www.meta.com/affiliates/DoBCMkXXQ",
+    "img_link": "./img/Games/499312376_699971669839664_4087023905876285759_n.webp",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
    {
     "name": "The Boys",
     "affiliate_link": "https://www.meta.com/affiliates/Hyx6QCeSn",
@@ -22,22 +38,6 @@ export const featureGames: GameSchema[] = [
     "name": "Beat the Beats",
     "affiliate_link": "https://www.meta.com/affiliates/7UHwub3Lp",
     "img_link": "./img/Games/38974887_903317764512651_4367060651741261931_n.webp",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-   {
-    "name": "Racket Pinball",
-    "affiliate_link": "https://www.meta.com/affiliates/8TXALubf9",
-    "img_link": "./img/Games/491824328_1336956178467458_2886774562409944223_n.webp",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-   {
-    "name": "Cursed Echoes",
-    "affiliate_link": "https://www.meta.com/affiliates/8TdmUjrRs",
-    "img_link": "./img/Games/491819983_1406065877290819_3050450984097403096_n.webp",
     "yt_link": "",
     "crossbuy": 0,
     "bhaptics": 0
