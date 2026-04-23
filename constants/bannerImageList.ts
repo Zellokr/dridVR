b08c80a86d77c289003e4d13a2852f65053d4bee
DriDGG <yeray1060@gmail.com>
2026-04-23 12:48:10 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -2,33 +2,30 @@ import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
 
+   {
+    image: "/img/Banner/theamusement_banner.webp",
+    altText: "Videojuego de realidad virtual llamado The Amusement",
+    url: "https://www.meta.com/affiliates/P6C3GMwVh",
+  },
  {
     image: "/img/Banner/banner_image_1.webp",
     altText: "Banner de videojuegos",
     url: "/games",
     contentId: "",
   },
-  {
-    image: "/img/Banner/WRATHBanner.webp",
-    altText: "Videojuego de realidad virtual llamado WRATH",
-    url: "https://www.meta.com/affiliates/8DmRM2WLP",
+   {
+    image: "/img/Banner/forefront_banner.webp",
+    altText: "Videojuego de realidad virtual llamado Forefront",
+    url: "https://www.meta.com/affiliates/7RdDupYuv",
   },
-    
-        {
+ {
     image: "/img/Banner/banner_image_2.webp",
     altText: "Banner de accesorios de realidad virtual",
     url: "https://www.meta.com/affiliates/AbzRrTmFY",
   },
-   {
-    image: "/img/Banner/DemeoDungeonAndDragonBanner.webp",
-    altText: "Videojuego de realidad virtual llamado Demeo",
-    url: "https://www.meta.com/affiliates/BQKF62DeV",
-  },
-     
      {
-    image: "/img/Banner/Deadpoolbanner.webp",
-    altText: "Videojuego de realidad virtual llamado Deadpool",
-    url: "https://www.meta.com/affiliates/4PdCTit3pS",
+    image: "/img/Banner/WRATHBanner.webp",
+    altText: "Videojuego de realidad virtual llamado WRATH",
+    url: "https://www.meta.com/affiliates/8DmRM2WLP",
   }
-
 ];
