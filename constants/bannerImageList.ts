923327feb2ed1e31a1574c36b7732c8011fbe12c
DriDGG <yeray1060@gmail.com>
2026-04-24 13:15:40 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -1,31 +1,32 @@
 import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
-
-   {
-    image: "/img/Banner/theamusement_banner.webp",
-    altText: "Videojuego de realidad virtual llamado The Amusement",
-    url: "https://www.meta.com/affiliates/P6C3GMwVh",
+ {
+    image: "/img/Banner/LittleNightmares_banner.webp",
+    altText: "Videojuego de realidad virtual llamado LittleNightmares",
+    url: "https://www.meta.com/affiliates/CEEyqa2Sy",
   },
+  
  {
     image: "/img/Banner/banner_image_1.webp",
     altText: "Banner de videojuegos",
     url: "/games",
     contentId: "",
   },
-   {
-    image: "/img/Banner/forefront_banner.webp",
-    altText: "Videojuego de realidad virtual llamado Forefront",
-    url: "https://www.meta.com/affiliates/7RdDupYuv",
+    {
+    image: "/img/Banner/theamusement_banner.webp",
+    altText: "Videojuego de realidad virtual llamado The Amusement",
+    url: "https://www.meta.com/affiliates/P6C3GMwVh",
   },
+   
  {
     image: "/img/Banner/banner_image_2.webp",
     altText: "Banner de accesorios de realidad virtual",
     url: "https://www.meta.com/affiliates/AbzRrTmFY",
   },
      {
-    image: "/img/Banner/WRATHBanner.webp",
-    altText: "Videojuego de realidad virtual llamado WRATH",
-    url: "https://www.meta.com/affiliates/8DmRM2WLP",
+    image: "/img/Banner/forefront_banner.webp",
+    altText: "Videojuego de realidad virtual llamado Forefront",
+    url: "https://www.meta.com/affiliates/7RdDupYuv",
   }
 ];
