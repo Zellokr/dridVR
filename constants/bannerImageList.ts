fd9bcd5195746c817f1efb26a1ba21db5a9310b7
DriDGG <yeray1060@gmail.com>
2026-05-07 18:39:01 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -2,28 +2,26 @@ import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
  {
+    image: "/img/Banner/evilinsidebanner.webp",
+    altText: "Videojuego de realidad virtual llamado Evil Inside VR",
+    url: "https://www.meta.com/affiliates/4bxt4yfrdi",
+  },
+ {
     image: "/img/Banner/tortugasninjas_banner.webp",
     altText: "Videojuego de realidad virtual llamado Teenage Mutant Ninja Turtles Empire City",
     url: "https://www.meta.com/affiliates/J2sjJUEU9",
   },
- {
-    image: "/img/Banner/LittleNightmares_banner.webp",
-    altText: "Videojuego de realidad virtual llamado LittleNightmares",
-    url: "https://www.meta.com/affiliates/CEEyqa2Sy",
-  },
-  
  {
     image: "/img/Banner/banner_image_1.webp",
     altText: "Banner de videojuegos",
     url: "/games",
     contentId: "",
   },
-    {
-    image: "/img/Banner/theamusement_banner.webp",
-    altText: "Videojuego de realidad virtual llamado The Amusement",
-    url: "https://www.meta.com/affiliates/P6C3GMwVh",
+   {
+    image: "/img/Banner/LittleNightmares_banner.webp",
+    altText: "Videojuego de realidad virtual llamado LittleNightmares",
+    url: "https://www.meta.com/affiliates/CEEyqa2Sy",
   },
-   
  {
     image: "/img/Banner/banner_image_2.webp",
     altText: "Banner de accesorios de realidad virtual",
