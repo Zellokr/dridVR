76fcf8b9f0abf4e33483ec4425715bd921c49b24
DriDGG <yeray1060@gmail.com>
2026-07-10 13:33:22 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -2,6 +2,30 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 
 export const featureGames: GameSchema[] = [
 
+      {
+    "name": "COLLISION CRASHER",
+    "affiliate_link": "https://www.meta.com/affiliates/8RXpHUWLz",
+    "img_link": "./img/Games/499619078_1718505519155276_8650386291820538248_n.webp",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+     {
+    "name": "Color Space",
+    "affiliate_link": "https://www.ftjcfx.com/click-101284762-15639798?url=https%3A%2F%2Fwww.meta.com%2Fexperiences%2F2702419963135346%2F&cjsku=2702419963135346&widgetid=67409c6d6e73e5d8cacefdc1",
+    "img_link": "./img/Games/39036706_682853542925536_3308804217065704261_n.jpg",
+    "yt_link": "",
+    "crossbuy": 0,
+    "bhaptics": 0
+  },
+     {
+    "name": "Downtown Club",
+    "affiliate_link": "https://www.meta.com/affiliates/Bxc4b21vf",
+    "img_link": "./img/Games/491823812_983948500925255_8374742121241205938_n.webp",
+    "yt_link": "https://youtu.be/_jVCKLxSi5E?si=ltUzfxE_HhzNMfgF",
+    "crossbuy": 0,
+    "bhaptics": 1
+  },
     {
     "name": "Compass",
     "affiliate_link": "https://www.meta.com/affiliates/Dcog9RGup",
@@ -15,30 +39,6 @@ export const featureGames: GameSchema[] = [
     "affiliate_link": "https://www.meta.com/affiliates/CsQ6RpYqY",
     "img_link": "./img/Games/491819356_1291497902486724_1697658662175617703_n.webp",
     "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-     {
-    "name": "Evil Inside VR",
-    "affiliate_link": "https://www.meta.com/affiliates/4bxt4yfrdi",
-    "img_link": "./img/Games/499303746_1395052968900213_2557845914393711386_n.webp",
-    "yt_link": "https://youtu.be/VkJ5ZIz-ZNc?si=Ov--xprZ3zYJ8hoP",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-     {
-    "name": "Teenage Mutant Ninja Turtles Empire City",
-    "affiliate_link": "https://www.meta.com/affiliates/J2sjJUEU9",
-    "img_link": "./img/Games/491820141_3007694096287200_6241673081806224560_n.webp",
-    "yt_link": "https://youtu.be/VDGB6CJpn-I?si=I_A4o4RNGMp6Ty2J",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-    {
-    "name": "Spymaster",
-    "affiliate_link": "https://www.meta.com/affiliates/Cknm3EzXD",
-    "img_link": "./img/Games/491823341_937103132073728_7336675141747262623_n.webp",
-    "yt_link": "https://youtu.be/j3Do5qC2sXY",
     "crossbuy": 0,
     "bhaptics": 0
   }
