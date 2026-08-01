2661935598b537d253ef3be2214df7253c817d58
DriDGG <yeray1060@gmail.com>
2026-08-01 11:06:55 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -3,26 +3,26 @@ import type { GameSchema } from "~/components/CustomCards/MediumCard/Card.vue";
 export const featureGames: GameSchema[] = [
 
       {
-    "name": "COLLISION CRASHER",
-    "affiliate_link": "https://www.meta.com/affiliates/8RXpHUWLz",
-    "img_link": "./img/Games/499619078_1718505519155276_8650386291820538248_n.webp",
-    "yt_link": "",
+    "name": "Quantum Void",
+    "affiliate_link": "https://www.meta.com/affiliates/9HVeYLPmR",
+    "img_link": "./img/Games/499304764_1068310528797317_7394462327346204458_n.webp",
+    "yt_link": "https://youtu.be/PMpe8XymeiQ?si=w5WD2fU8sgAIMp18",
+    "crossbuy": 0,
+    "bhaptics": 1
+  },
+    {
+    "name": "Roboquest VR",
+    "affiliate_link": "https://www.meta.com/affiliates/9LhvLsaVy",
+    "img_link": "./img/Games/499620504_1704268167442117_6927292149698847991_n.webp",
+    "yt_link": "https://youtu.be/u9MITkUrg7U?si=GPXLtHBYtiUkzU1x",
     "crossbuy": 0,
     "bhaptics": 0
   },
-     {
-    "name": "Color Space",
-    "affiliate_link": "https://www.ftjcfx.com/click-101284762-15639798?url=https%3A%2F%2Fwww.meta.com%2Fexperiences%2F2702419963135346%2F&cjsku=2702419963135346&widgetid=67409c6d6e73e5d8cacefdc1",
-    "img_link": "./img/Games/39036706_682853542925536_3308804217065704261_n.jpg",
-    "yt_link": "",
-    "crossbuy": 0,
-    "bhaptics": 0
-  },
-     {
-    "name": "Downtown Club",
-    "affiliate_link": "https://www.meta.com/affiliates/Bxc4b21vf",
-    "img_link": "./img/Games/491823812_983948500925255_8374742121241205938_n.webp",
-    "yt_link": "https://youtu.be/_jVCKLxSi5E?si=ltUzfxE_HhzNMfgF",
+      {
+    "name": "Ghosts of Tabor: LEGACY",
+    "affiliate_link": "https://www.meta.com/affiliates/JZznCwJ3E",
+    "img_link": "./img/Games/499622315_1046518557807389_6281357192614672138_n.webp",
+    "yt_link": " https://youtu.be/R27HzTfxUWo?si=qBVMKd1hDlIAuYNT",
     "crossbuy": 0,
     "bhaptics": 1
   },
