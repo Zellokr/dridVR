d4e11dde84b08fdc8f0d2d2ef4e74f27e6d5d7d8
DriDGG <yeray1060@gmail.com>
2026-08-01 11:05:32 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -1,11 +1,7 @@
 import type { DataImage } from "~/components/Body/Banner/types";
 
 export const bannerImagesList: DataImage[] = [
- {
-    image: "/img/Banner/evilinsidebanner.webp",
-    altText: "Videojuego de realidad virtual llamado Evil Inside VR",
-    url: "https://www.meta.com/affiliates/4bxt4yfrdi",
-  },
+
  {
     image: "/img/Banner/quantumvoid_banner.webp",
     altText: "Videojuego de realidad virtual llamado Quantum Void",
@@ -26,6 +22,11 @@ export const bannerImagesList: DataImage[] = [
     image: "/img/Banner/banner_image_2.webp",
     altText: "Banner de accesorios de realidad virtual",
     url: "https://www.meta.com/affiliates/AbzRrTmFY",
+  },
+  {
+    image: "/img/Banner/evilinsidebanner.webp",
+    altText: "Videojuego de realidad virtual llamado Evil Inside VR",
+    url: "https://www.meta.com/affiliates/4bxt4yfrdi",
   },
      
 ];
