93153d8e7859bf57127f65338ed0b1c63df8f751
DriDGG <yeray1060@gmail.com>
2026-09-25 23:50:05 +0100

    Update bannerImageList.ts

--- constants/bannerImageList.ts
+++ constants/bannerImageList.ts
@@ -3,9 +3,9 @@ import type { DataImage } from "~/components/Body/Banner/types";
 export const bannerImagesList: DataImage[] = [
 
  {
-    image: "/img/Banner/quantumvoid_banner.webp",
-    altText: "Videojuego de realidad virtual llamado Quantum Void",
-    url: "https://www.meta.com/affiliates/9HVeYLPmR",
+    image: "/img/Banner/TheLightsKeeper_banner.webp",
+    altText: "Videojuego de realidad virtual llamado The Lightkeepers",
+    url: "https://metacreator.pxf.io/PzdMxq",
   },
  {
     image: "/img/Banner/banner_image_1.webp",
@@ -16,17 +16,17 @@ export const bannerImagesList: DataImage[] = [
    {
     image: "/img/Banner/roboquest_banner.webp",
     altText: "Videojuego de realidad virtual llamado Roboquest VR",
-    url: "https://www.meta.com/affiliates/9LhvLsaVy",
+    url: "https://metacreator.pxf.io/xJ6MNy",
   },
  {
     image: "/img/Banner/banner_image_2.webp",
     altText: "Banner de accesorios de realidad virtual",
-    url: "https://www.meta.com/affiliates/AbzRrTmFY",
+    url: "https://metacreator.pxf.io/L0d6yM",
   },
   {
     image: "/img/Banner/evilinsidebanner.webp",
     altText: "Videojuego de realidad virtual llamado Evil Inside VR",
-    url: "https://www.meta.com/affiliates/4bxt4yfrdi",
-  },
+    url: "https://metacreator.pxf.io/3k0N6y",
+  }
      
 ];
