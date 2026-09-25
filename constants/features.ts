c5f7c8feded9ae8215698bffd6107961d5f57782
DriDGG <yeray1060@gmail.com>
2026-09-25 17:44:03 +0100

    Update features.ts

--- constants/features.ts
+++ constants/features.ts
@@ -4,7 +4,7 @@ export const featureGames: GameSchema[] = [
 
       {
     "name": "Quantum Void",
-    "affiliate_link": "https://www.meta.com/affiliates/9HVeYLPmR",
+    "affiliate_link": "https://metacreator.pxf.io/PzdxKj",
     "img_link": "./img/Games/499304764_1068310528797317_7394462327346204458_n.webp",
     "yt_link": "https://youtu.be/PMpe8XymeiQ?si=w5WD2fU8sgAIMp18",
     "crossbuy": 0,
@@ -12,7 +12,7 @@ export const featureGames: GameSchema[] = [
   },
     {
     "name": "Roboquest VR",
-    "affiliate_link": "https://www.meta.com/affiliates/9LhvLsaVy",
+    "affiliate_link": "https://metacreator.pxf.io/xJ6MNy",
     "img_link": "./img/Games/499620504_1704268167442117_6927292149698847991_n.webp",
     "yt_link": "https://youtu.be/u9MITkUrg7U?si=GPXLtHBYtiUkzU1x",
     "crossbuy": 0,
@@ -20,7 +20,7 @@ export const featureGames: GameSchema[] = [
   },
       {
     "name": "Ghosts of Tabor: LEGACY",
-    "affiliate_link": "https://www.meta.com/affiliates/JZznCwJ3E",
+    "affiliate_link": "https://metacreator.pxf.io/qW6d4g",
     "img_link": "./img/Games/499622315_1046518557807389_6281357192614672138_n.webp",
     "yt_link": " https://youtu.be/R27HzTfxUWo?si=qBVMKd1hDlIAuYNT",
     "crossbuy": 0,
@@ -28,7 +28,7 @@ export const featureGames: GameSchema[] = [
   },
     {
     "name": "Compass",
-    "affiliate_link": "https://www.meta.com/affiliates/Dcog9RGup",
+    "affiliate_link": "https://metacreator.pxf.io/yZ61Y3",
     "img_link": "./img/Games/499300661_853052124524832_8811125998914025448_n.webp",
     "yt_link": "",
     "crossbuy": 0,
@@ -36,7 +36,7 @@ export const featureGames: GameSchema[] = [
   },
      {
     "name": "Virtual Hunter",
-    "affiliate_link": "https://www.meta.com/affiliates/CsQ6RpYqY",
+    "affiliate_link": "https://metacreator.pxf.io/jR6bD5",
     "img_link": "./img/Games/491819356_1291497902486724_1697658662175617703_n.webp",
     "yt_link": "",
     "crossbuy": 0,
